b246e6de69c1719f165dec48cd219a2b7638bc92
JiaqiLiang <ljq@JiaqiLiangs-MBP.lan>
2019-04-26 22:01:48 -0700

    change to springboot

---
+++ src/main/java/com/ljqiii/BookSnippetsHubJApplication.java
@@ -0,0 +1,13 @@
+package com.ljqiii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSnippetsHubJApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookSnippetsHubJApplication.class, args);
+    }
+
+}
