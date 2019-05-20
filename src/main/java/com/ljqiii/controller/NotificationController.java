2e9328784b6468f7fc18ea2c517f82d7ff84012e
Ljqiii <ljq917181927@gmail.com>
2019-05-19 17:30:16 -0700

    notification ok

---
+++ src/main/java/com/ljqiii/controller/NotificationController.java
@@ -0,0 +1,36 @@
+package com.ljqiii.controller;
+
+
+import com.alibaba.fastjson.JSONObject;
+import com.ljqiii.config.security.GrantedAuthority.WxAuthenticationToken;
+import com.ljqiii.model.WxAccount;
+import com.ljqiii.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class Notification {
+
+    @Autowired
+    NotificationService notificationService;
+
+
+    @PreAuthorize("hasAuthority('ROLE_WXUSER')")
+    @GetMapping("/getnotification")
+    public ArrayList<JSONObject> getmsg(WxAuthenticationToken wxAuthenticationToken) {
+        WxAccount wxAccount= (WxAccount) wxAuthenticationToken.getPrincipal();
+
+//        JSONObject responejson = new JSONObject();
+        ArrayList<JSONObject> notifications = notificationService.getAllNotificationByToOpenid(wxAccount.getOpenId());
+
+        return notifications;
+
+
+    }
+
+
+}
